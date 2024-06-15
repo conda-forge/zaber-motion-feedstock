1a0c584bb96389fcb06ad396485355f17311ef8e
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-15 08:03:57 -0400

    Update build_python.sh

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -3,9 +3,6 @@ cd src/zaber-motion
 
 pushd py
 
-# without the __init__ file, it won't install correctly
-touch zaber_motion/protobufs/__init__.py
-
 $PYTHON $RECIPE_DIR/filter_setup.py
 $PYTHON -m pip install . -vv
 popd
