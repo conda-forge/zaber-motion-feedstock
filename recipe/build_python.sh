d5f76441784e4edc4044cd4f7fc41a321153a252
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-15 08:03:48 -0400

    Update build_python.sh

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -1,11 +1,6 @@
 set -ex
 cd src/zaber-motion
 
-# Now compile the python bindings
-protoc -I=. --python_out="py/zaber_motion" \
-  --plugin="protoc-gen-mypy=tools/protoc-gen-mypy" \
-  --mypy_out="py/zaber_motion" protobufs/main.proto
-
 pushd py
 
 # without the __init__ file, it won't install correctly
